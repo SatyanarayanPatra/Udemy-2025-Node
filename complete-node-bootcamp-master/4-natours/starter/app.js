ac8db3b51abc3c2634d2fc7f5d07fcd9a7284487
S Satyanarayana Patra <satya77888narayanpatra@gmail.com>
2025-03-12 21:14:35 +0530

    get single tour using id

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -32,6 +32,17 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params.id);
+  const tour = tours.filter((el) => el.id === +req.params.id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tour[0],
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = req.body;
@@ -39,12 +50,12 @@ app.post('/api/v1/tours', (req, res) => {
   tours.push(newTour);
   const data = JSON.stringify(tours);
   fs.writeFile('./dev-data/data/tours-simple.json', data, (err) => {
-		res.status(201).json({
-			status: 'success',
-			data: {
-				tour: newTour,
-			},
-		});
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
   });
 });
 
