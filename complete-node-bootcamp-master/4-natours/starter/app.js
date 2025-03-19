c8707c6277cedbe28b8166843faa1867d83f57b0
S Satyanarayana Patra <satya77888narayanpatra@gmail.com>
2025-03-20 03:24:30 +0530

    changes added

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -9,6 +9,7 @@ const port = 3000;
 // 2 - MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
